2e217da84c4f9a25fecb0fce76f069fbc4b04b77
marcinch18 <mchilik@gmail.com>
2018-04-06 18:05:24 +0100

    Add files via upload

---
+++ blockproducer.py
@@ -0,0 +1,56 @@
+import mainConfig
+import json
+
+class BlockProducer:
+    def __init__(self, **kwargs):
+        self.node_addr=kwargs.get('node_addr')
+        self.port_p2p=kwargs.get('port_p2p')
+        self.port_http=kwargs.get('port_http')
+        self.port_ssl=kwargs.get('port_ssl')
+        self.block_signing_key=kwargs.get('block_signing_key')
+        self.organisation=kwargs.get('organisation')
+        self.account_name=kwargs.get('account_name')
+        self.location=kwargs.get('location')
+
+
+    def genP2Pconf(self):
+        return "p2p-peer-address = " + str(self.node_addr) + ":".join([str(self.node_addr),str(self.port_p2p)])
+
+    def getBPjson(self):
+        bpdata =   {
+                "account_name": str(self.account_name),
+                "organisation": str(self.organisation),
+                "location": str(self.location),
+                "node_addr": str(self.node_addr),
+                "port_http": str(self.port_http),
+                "port_ssl": str(self.port_ssl),
+                "port_p2p": str(self.port_p2p),
+                "block_signing_key": str(self.block_signing_key)
+                }
+
+        return json.dumps(bpdata, ensure_ascii=False)
+
+    def setProducerParameters(self,confDic):
+        for key in confDic:
+            if self.__dict__.has_key(key):
+                self.__dict__[key] = confDic[key]
+
+
+class netProcuders(BlockProducer):
+    def __init__(self,producers):
+        self.producers = producers
+
+
+producersParametersList = mainConfig.blockProducerList
+p2pList = ""
+with open("config.ini",'w') as f:
+    for producerParameter in producersParametersList:
+        bp = BlockProducer()
+        bp.setProducerParameters(producerParameter)
+        
+        bp.genP2Pconf()
+        f.write(bp.genP2Pconf())
+        f.write("\n")
+f.close()
+#with open('data.txt', 'w') as f:
+    #json.dump(data, f, ensure_ascii=False)
---
+++ mainConfig.py
@@ -0,0 +1,84 @@
+blockProducerList = [
+  {
+    "account_name": "juggernaut",
+    "organisation": "EOSgreen.io",
+    "location": "UK",
+    "node_addr": "eosgreen.uk.to",
+    "port_http": "8889",
+    "port_ssl": "",
+    "port_p2p": "9875",
+    "block_signing_key": "EOS8LAhzKRo5HiaR83tP8AaYHogQAKq8L8GKNm9eSi9nj6ze6cEzC"
+  },
+  {
+    "account_name": "storm",
+    "organisation": "EOSIO.se",
+    "location": "Vasteras, Sweden",
+    "node_addr": "ctestnet.eosio.se",
+    "port_http": "8887",
+    "port_ssl": "",
+    "port_p2p": "9875",
+    "block_signing_key": "EOS55xgWhiYohaHtFTxcPMorB9MzwBfe3JgUuQEujrurmHLHiXmna"
+  },
+  {
+    "account_name": "batman",
+    "organisation": "eosDAC.io",
+    "location": "Ireland",
+    "node_addr": "54.194.49.21",
+    "port_http": "8833",
+    "port_ssl": "",
+    "port_p2p": "9875",
+    "block_signing_key": "EOS5wEcKNZ8YdVnCSCnGmwpWLwb2cCqP5FSyWoW1vBASr78KKu8G3"
+  },
+  {
+    "account_name": "venom",
+    "organisation": "EOS Cafe",
+    "location": "Canada, Calgary",
+    "node_addr": "venom.eoscalgary.com",
+    "port_http": "80",
+    "port_ssl": "",
+    "port_p2p": "9877",
+    "block_signing_key": "EOS5Xi3QA8rtbrzLFgma7azYigMxFoGYij2j7ZmjwXjmnHrtAbkGk"
+  },
+  {
+    "account_name": "joker",
+    "organisation": "RoelandP.nl/eos",
+    "location": "Amsterdam and Germany",
+    "node_addr": "joker.superhero.eos.roelandp.nl",
+    "port_http": "8763",
+    "port_ssl": "",
+    "port_p2p": "9873",
+    "block_signing_key": "EOS5a37GWFxp74YGGDr6oFRoFjGrtvdYk8vbyid2JQ9EBBSSLzbNz"
+
+  },
+  {
+    "account_name": "hellboy",
+    "organisation": "EOS Detroit",
+    "location": "US,Seattle",
+    "node_addr": "ctestnet.eosdetroit.com",
+    "port_http": "8889",
+    "port_ssl": "",
+    "port_p2p": "1339",
+    "block_signing_key": "EOS75pHe7xQGQZEjXGd8yCDomKV3whFYr5idz9JmAVHST7t67H3gg"
+  },
+  {
+    "account_name": "cyclops",
+    "organisation": "EOS42",
+    "location": "UK,London",
+    "node_addr": "bp7-d3.eos42.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS7HCLs7UkXJhjvzm9nu7RzMbPU99HhDqZdbt3a86CGV2uDqAQW5"
+  },
+  {
+    "account_name": "wakanda",
+    "organisation": "EOS Africa",
+    "location": "South Africa,Johannesburg",
+    "node_addr": "superheroes.eosio.africa",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS6p2RxAzMD8QuVEr75bnvJk3XDrZVYUkyEiuTSj3Qhyp2vWmfFD"
+  },
+
+]
