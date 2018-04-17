1622f04557c23def04a20d8a9cc6310005374b3a
marcinch18 <mchilik@gmail.com>
2018-04-17 21:52:31 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -367,8 +367,19 @@ var blockProducerList = [
     "port_ssl": "",
     "port_p2p": "9876",
     "block_signing_key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
+  },
+  {
+    "bp_name": "spaceghost",
+    "organisation": "Worbli",
+    "location": "USA,Ohio",
+    "node_addr": "superhero.worbli.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS6NwJ63CcMKdG4qwa4Nc3qoW2PmartGbizCGrG9tTWyQVfWwKUG"
   }
 
 
 
 ]
+
