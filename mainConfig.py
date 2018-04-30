d4580c4ac497334a84a10d49d92df04e6c8a90df
marcinch18 <mchilik@gmail.com>
2018-04-30 20:33:44 +0100

    Add files via upload

--- mainConfig.py
+++ mainConfig.py
@@ -1,6 +1,6 @@
 blockProducerList = [
   {
-    "account_name": "juggernaut",
+    "bp_name": "juggernaut",
     "organisation": "EOSgreen.io",
     "location": "UK",
     "node_addr": "eosgreen.uk.to",
@@ -10,17 +10,17 @@ blockProducerList = [
     "block_signing_key": "EOS8LAhzKRo5HiaR83tP8AaYHogQAKq8L8GKNm9eSi9nj6ze6cEzC"
   },
   {
-    "account_name": "storm",
+    "bp_name": "storm",
     "organisation": "EOSIO.se",
     "location": "Vasteras, Sweden",
-    "node_addr": "ctestnet.eosio.se",
-    "port_http": "8887",
+    "node_addr": "ctestnet.edenx.io",
+    "port_http": "8888",
     "port_ssl": "",
-    "port_p2p": "9875",
+    "port_p2p": "62071",
     "block_signing_key": "EOS55xgWhiYohaHtFTxcPMorB9MzwBfe3JgUuQEujrurmHLHiXmna"
   },
   {
-    "account_name": "batman",
+    "bp_name": "batman",
     "organisation": "eosDAC.io",
     "location": "Ireland",
     "node_addr": "54.194.49.21",
@@ -30,7 +30,17 @@ blockProducerList = [
     "block_signing_key": "EOS5wEcKNZ8YdVnCSCnGmwpWLwb2cCqP5FSyWoW1vBASr78KKu8G3"
   },
   {
-    "account_name": "venom",
+    "bp_name": "tom",
+    "organisation": "CryptoLions.Io",
+    "location": "Ukraine",
+    "node_addr": "superhero.cryptolions.io",
+    "port_http": "8897",
+    "port_ssl": "",
+    "port_p2p": "9885",
+    "block_signing_key": "EOS5DLvsuc8fmbgn2vQJDhd7ghgRyWuAPmjDaJ7SRagKV5rqDS1ph"
+  },
+  {
+    "bp_name": "venom",
     "organisation": "EOS Cafe",
     "location": "Canada, Calgary",
     "node_addr": "venom.eoscalgary.com",
@@ -40,7 +50,7 @@ blockProducerList = [
     "block_signing_key": "EOS5Xi3QA8rtbrzLFgma7azYigMxFoGYij2j7ZmjwXjmnHrtAbkGk"
   },
   {
-    "account_name": "joker",
+    "bp_name": "joker",
     "organisation": "RoelandP.nl/eos",
     "location": "Amsterdam and Germany",
     "node_addr": "joker.superhero.eos.roelandp.nl",
@@ -51,9 +61,9 @@ blockProducerList = [
 
   },
   {
-    "account_name": "hellboy",
+    "bp_name": "hellboy",
     "organisation": "EOS Detroit",
-    "location": "USA,Seattle",
+    "location": "US,Seattle",
     "node_addr": "ctestnet.eosdetroit.com",
     "port_http": "8889",
     "port_ssl": "",
@@ -61,7 +71,7 @@ blockProducerList = [
     "block_signing_key": "EOS75pHe7xQGQZEjXGd8yCDomKV3whFYr5idz9JmAVHST7t67H3gg"
   },
   {
-    "account_name": "cyclops",
+    "bp_name": "cyclops",
     "organisation": "EOS42",
     "location": "UK,London",
     "node_addr": "bp7-d3.eos42.io",
@@ -71,7 +81,7 @@ blockProducerList = [
     "block_signing_key": "EOS7HCLs7UkXJhjvzm9nu7RzMbPU99HhDqZdbt3a86CGV2uDqAQW5"
   },
   {
-    "account_name": "wakanda",
+    "bp_name": "wakanda",
     "organisation": "EOS Africa",
     "location": "South Africa,Johannesburg",
     "node_addr": "superheroes.eosio.africa",
@@ -81,7 +91,17 @@ blockProducerList = [
     "block_signing_key": "EOS6p2RxAzMD8QuVEr75bnvJk3XDrZVYUkyEiuTSj3Qhyp2vWmfFD"
   },
   {
-    "account_name": "spiderman",
+    "bp_name": "wakanda2",
+    "organisation": "EOS Africa",
+    "location": "Africa",
+    "node_addr": "156.38.160.91",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS6p2RxAzMD8QuVEr75bnvJk3XDrZVYUkyEiuTSj3Qhyp2vWmfFD"
+  },
+  {
+    "bp_name": "spiderman",
     "organisation": "SaltBlock",
     "location": "USA,UT,Salt Lake City",
     "node_addr": "166.70.202.194",
@@ -91,7 +111,7 @@ blockProducerList = [
     "block_signing_key": "EOS8dzxm4HAk8uRJawAWzAJZ6nj4uBCx3bbjBFxzzskHpQhSRELe7"
   },
   {
-    "account_name": "jigsaw",
+    "bp_name": "jigsaw",
     "organisation": "Blockpro",
     "location": "USA, Ohio",
     "node_addr": "18.188.52.250",
@@ -101,9 +121,9 @@ blockProducerList = [
     "block_signing_key": "EOS5AJLcPVhq9zgHvTKgdWd76R943cuUUybpYt7JdrT5BV4T9je12"
   },
   {
-    "account_name": "ironman",
+    "bp_name": "ironman",
     "organisation": "EOS New York",
-    "location": "USA,Virgina",
+    "location": "USA, Virgina",
     "node_addr": "ctest.eosnewyork.io",
     "port_http": "8887",
     "port_ssl": "",
@@ -111,17 +131,17 @@ blockProducerList = [
     "block_signing_key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
   },
   {
-    "account_name": "hook",
+    "bp_name": "hook",
     "organisation": "EOS Denmark",
     "location": "Belgium",
     "node_addr": "35.195.161.56",
     "port_http": "8888",
     "port_ssl": "",
     "port_p2p": "9876",
-    "block_signing_key": "EOS8b7CU2eJG9Z4aVkx6guoJJouKcSqt4LLVJR32rpZZw1GeyhJvd"
+    "block_signing_key": ""
   },
   {
-    "account_name": "robin",
+    "bp_name": "robin",
     "organisation": "OracleChain",
     "location": "Singapore",
     "node_addr": "159.89.197.162",
@@ -131,7 +151,7 @@ blockProducerList = [
     "block_signing_key": "EOS7jtFqvghBxvs6EvaQyWWf9vxdmWtTetACiy8PXqxtxXGrHfGVr"
   },
   {
-    "account_name": "rogue",
+    "bp_name": "rogue",
     "organisation": "Tokenika",
     "location": "Poland",
     "node_addr": "dawn3-seed.tokenika.io",
@@ -141,7 +161,7 @@ blockProducerList = [
     "block_signing_key": "EOS711cqTKFYXXk5nGPgySUy6U34CNBhCcCwaD2PwZ14GJXu1Anwt"
   },
   {
-    "account_name": "daredevil",
+    "bp_name": "daredevil",
     "organisation": "Blockgenic",
     "location": "USA, West Coast",
     "node_addr": "bp.blockgenic.io",
@@ -151,7 +171,7 @@ blockProducerList = [
     "block_signing_key": "EOS7ggmHeJpysDVBHahjHr3Rq46eGxJx9wtbTsmPEvJcGSfMic1uf"
   },
   {
-    "account_name": "magneto",
+    "bp_name": "magneto",
     "organisation": "EOS Gravity",
     "location": "China",
     "node_addr": "47.52.18.70",
@@ -161,7 +181,7 @@ blockProducerList = [
     "block_signing_key": "EOS5D5P25LN3pkMsvRJ1a8vYh9CFd2u4MHLSmfcYnugvTEcN38vPm"
   },
   {
-    "account_name": "doom",
+    "bp_name": "doom",
     "organisation": "91EOS",
     "location": "China",
     "node_addr": "120.27.130.60",
@@ -171,7 +191,7 @@ blockProducerList = [
     "block_signing_key": "EOS8NnpE3hVrBdE6JhaMQdxSYAdZqR9qRyg5o3XrEDhCeB6mwBtYr"
   },
   {
-    "account_name": "megatron",
+    "bp_name": "megatron",
     "organisation": "koreos_io",
     "location": "South Korea",
     "node_addr": "ctest.koreos.io",
@@ -181,7 +201,7 @@ blockProducerList = [
     "block_signing_key": "EOS81LJmHkomMihCyrZ3W5btVqNkiLg7zvYEdKF516UNwBCN2eAN4"
   },
   {
-    "account_name": "superman",
+    "bp_name": "superman",
     "organisation": "OCI",
     "location": "USA,St. Louis",
     "node_addr": "ctestnet.objectcomputing.com",
@@ -191,17 +211,7 @@ blockProducerList = [
     "block_signing_key": "EOS8kJXy2zWbyrmKA8DXg7ubQMjpuDUQKjdpc39SmbeKqjkCi8SXa"
   },
   {
-    "account_name": "hulk",
-    "organisation": "EOSphere",
-    "location": "Australia,Perth",
-    "node_addr": "superhero-bp1.eosphere.io",
-    "port_http": "8888",
-    "port_ssl": "",
-    "port_p2p": "9876",
-    "block_signing_key": "EOS8Zj999MDCzMo1gDuyieFCMMsHKyfPxUB4towsmbjXXQShsFA5a"
-  },
-  {
-    "account_name": "beast",
+    "bp_name": "beast",
     "organisation": "EOSYS",
     "location": "Korea,Seoul",
     "node_addr": "test.eosys.io",
@@ -212,9 +222,9 @@ blockProducerList = [
   },
   {
     "bp_name": "stealth",
-    "organisation": "Stealth",
+    "organisation": "EOS Asia",
     "location": "Korea,Seoul",
-    "node_addr": "stealthbp.ddns.net",
+    "node_addr": "bp-test.eosasia.one",
     "port_http": "8888",
     "port_ssl": "",
     "port_p2p": "9876",
@@ -259,6 +269,116 @@ blockProducerList = [
     "port_ssl": "",
     "port_p2p": "9877",
     "block_signing_key": "EOS6hxModmy1ohpVhQ578xcvCZW78Xb8RK6P5iCwMzNzUuXCA86Y1"
+  },
+  {
+    "bp_name": "aquaman",
+    "organisation": "EOShenzhen",
+    "location": "China,ShenZhen",
+    "node_addr": "ctestnet.eoshenzhen.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS64Vgd3PVJfoTvB19K8fShiy9e1ZGo1iN33PgLrJfUDeTXSmSuG"
+  },
+  {
+    "bp_name": "elektra",
+    "organisation": "enjoyshare",
+    "location": "China,ShenZhen",
+    "node_addr": "eosbp.enjoyshare.net",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS78M4WBz48hUfwQ14yceBvaS4xZGDyaoyGM3bsLxggzvPHHcAC"
+  },
+  {
+    "bp_name": "skeletor",
+    "organisation": "EOSBIXIN",
+    "location": "Thailand,Bangkok",
+    "node_addr": "bpt1.eosbixin.com",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS5DYrhVScCWEGiJJur2iYAhqxnUxWwgzYHjoBVoTRHomPUCUMRZ"
+  },
+  {
+    "bp_name": "gargamel",
+    "organisation": "www.bitcoineos.fun",
+    "location": "Spain",
+    "node_addr": "46.4.253.242",
+    "port_http": "7615",
+    "port_ssl": "",
+    "port_p2p": "7610",
+    "block_signing_key": "EOS8XtxmodhhtNizaJiRtc9941KFHxiHbG5sKTby8jW6cxFiXZDLe"
+  },
+  {
+    "bp_name": "hulk",
+    "organisation": "EOSphere",
+    "location": "Australia,Perth",
+    "node_addr": "superhero-bp1.eosphere.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS8Zj999MDCzMo1gDuyieFCMMsHKyfPxUB4towsmbjXXQShsFA5a"
+  },
+  {
+    "bp_name": "xavier",
+    "organisation": "EOSBR",
+    "location": "USA, CA",
+    "node_addr": "138.68.238.129",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9875",
+    "block_signing_key": "EOS6okFXLDor3y83hmPAbePAgpur5ATyKVuS7fxMSgSm2xs1SZTwT"
+  },
+  {
+    "bp_name": "batgirl",
+    "organisation": "EOS Novosibirsk",
+    "location": "Russia, Novosibirsk",
+    "node_addr": "178.49.174.48",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS8bvNf6GZeJRV3a62jU4vTL6MtQ9o3byFdS93jifMYj2CaaFvaV"
+  },
+  {
+    "bp_name": "spaceghost",
+    "organisation": "Worbil",
+    "location": "USA, Ohio",
+    "node_addr": "superhero.worbli.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS6NwJ63CcMKdG4qwa4Nc3qoW2PmartGbizCGrG9tTWyQVfWwKUG"
+  },
+  {
+    "bp_name": "wonderwoman",
+    "organisation": "EOSREAL",
+    "location": "India,Mumbai-AWS",
+    "node_addr": "wonderwoman.eosreal.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
+  },
+  {
+    "bp_name": "phoenix",
+    "organisation": "EOSBrazil",
+    "location": "Brazil,Joinville",
+    "node_addr": "eosbrazil.com",
+    "port_http": "8891",
+    "port_ssl": "",
+    "port_p2p": "9878",
+    "block_signing_key": "EOS62ZdqFkz4yWhdF3vE2AzMLDSRgEqSfLm5NESkabGY6V4HQ8PkB"
+  },
+  {
+    "bp_name": "loki",
+    "organisation": "spear.fund",
+    "location": "Ohio,Dayton",
+    "node_addr": "35.202.41.160",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876",
+    "block_signing_key": "EOS8kPE11KY4jBHNEPUnhDfdGwmpZT68YjtjtdRcNnKJjN2b7R1KQ"
   }
 
 ]
